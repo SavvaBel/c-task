84124d480777623f3050e646c48efae0e65f7cb3
Savva <savvabell11@gmail.com>
2023-03-07 17:03:19 +0300

    дз задачи 19 21 23

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -164,15 +164,142 @@
 // -4, 16 -> да
 // 25, 5 -> да
 // 8,9 -> нет
-Console.WriteLine("Ввести первое число ");
-int A = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Ввести второе число ");
-int B = Convert.ToInt32(Console.ReadLine());
-if(A == B*B || B == A*A)
+// Console.WriteLine("Ввести первое число ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Ввести второе число ");
+// int B = Convert.ToInt32(Console.ReadLine());
+// if(A == B*B || B == A*A)
+// {
+//     Console.WriteLine("Кратно");
+// }
+// else
+// {
+//     Console.WriteLine("Некратно");
+// }
+
+
+// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+// Console.Write("Введите x: ");
+// int x = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите y: ");
+// int y = int.Parse(Console.ReadLine());
+
+// if( x>0 && y>0 )
+// {
+//     Console.WriteLine("1");
+// }
+// if( x<0 && y>0 )
+// {
+//     Console.WriteLine("2");
+// }if( x<0 && y<0 )
+// {
+//     Console.WriteLine("3");
+// }if( x>0 && y<0 )
+// {
+//     Console.WriteLine("4");
+// }
+
+
+
+// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y)
+
+// Console.Write("Введите четверть: ");
+
+// int x = int.Parse(Console.ReadLine());
+// if (x == 1)
+// {
+//     Console.WriteLine("Диапозон X от 0 до 5, диапозон Y от 0 до 5");
+// }
+// if (x == 2)
+// {
+//     Console.WriteLine("Диапозон X от 0 до -5, диапозон Y от 0 до 5");
+// }
+// if (x == 3)
+// {
+//     Console.WriteLine("Диапозон X от 0 до -5, диапозон Y от 0 до -5");
+// }
+// if (x == 4)
+// {
+//     Console.WriteLine("Диапозон X от 0 до 5, диапозон Y от 0 до -5");
+// }
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+// Console.Write("Введите координаты Х1: ");
+// int x1 = int.Parse(Console.ReadLine());
+// Console.Write("Введите координаты У1: ");
+// int y1 = int.Parse(Console.ReadLine());
+// Console.Write("Введите координаты Х2: ");
+// int x2 = int.Parse(Console.ReadLine());
+// Console.Write("Введите координаты У2: ");
+// int y2 = int.Parse(Console.ReadLine());
+// int A = (x2-x1)*(x2-x1);
+// int B = (y2-y1)*(y2-y1);
+// double AB = Math.Sqrt(A+B);
+// Console.Write(AB);
+
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+
+// Console.Write("Введите число: ");
+// int N = int.Parse(Console.ReadLine());
+// int x = 1;
+// while (x <= N)
+// {
+//     int y = x*x;
+//     Console.WriteLine(y);
+//     x++;
+// }
+
+
+namespace CA_9
 {
-    Console.WriteLine("Кратно");
-}
-else
-{
-    Console.WriteLine("Некратно");
+    class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(IsPalindrome(num));
+            Console.ReadKey(true);
+        }
+        
+        static bool IsPalindrome(int num)
+        {
+            if (num >= 0 && num <10)
+                return true;
+            int numLength = GetLength(num);
+            int[] digits = new int[numLength];
+            for (int i = numLength - 1; i >= 0; i--) {
+                digits[i] = num % 10;
+                num /= 10;
+            }
+            for (int i = 0; i < numLength/2; i++) {
+                if (digits[i] != digits[numLength - i - 1])
+                    return false;
+            }
+            return true;
+        }
+        
+        static int GetLength(int num)
+        {
+            int n = 0;
+            while (num > 0) {
+                num /= 10;
+                n++;
+            }
+            return n;
+        }
+    }
 }
--- task10D/z/Program.cs
+++ task10D/z/Program.cs
@@ -122,30 +122,125 @@
 // 7 -> да
 // 1 -> нет
 
-Console.Write("Введите число от 1-7, где 1-5 будние дни; 6-7 выходные: ");
-int number = int.Parse(Console.ReadLine()!);
-if (number == 1) {
-System.Console.WriteLine("нет, будний");
-}if (number == 2) {
-System.Console.WriteLine("нет, будний");
-}
-if (number == 3)
-{
-System.Console.WriteLine("нет, будний");
-}
-if (number == 4)
-{
-System.Console.WriteLine("нет, будний");
-}
-if (number == 5)
-{
-System.Console.WriteLine("нет, будний");
-}
-if (number == 6)
-{
-System.Console.WriteLine("да, выходной");
-}
-if (number == 7)
-{
-System.Console.WriteLine("да, выходной");
-}
+// Console.Write("Введите число от 1-7, где 1-5 будние дни; 6-7 выходные: ");
+// int number = int.Parse(Console.ReadLine()!);
+// if (number == 1) {
+// System.Console.WriteLine("нет, будний");
+// }if (number == 2) {
+// System.Console.WriteLine("нет, будний");
+// }
+// if (number == 3)
+// {
+// System.Console.WriteLine("нет, будний");
+// }
+// if (number == 4)
+// {
+// System.Console.WriteLine("нет, будний");
+// }
+// if (number == 5)
+// {
+// System.Console.WriteLine("нет, будний");
+// }
+// if (number == 6)
+// {
+// System.Console.WriteLine("да, выходной");
+// }
+// if (number == 7)
+// {
+// System.Console.WriteLine("да, выходной");
+// }
+
+// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+
+// namespace CA_9
+// {
+//     class Program
+//     {
+ 
+//         static void Main(string[] args)
+//         {
+//             int num = int.Parse(Console.ReadLine());
+//             Console.WriteLine(IsPalindrome(num));
+//             Console.ReadKey(true);
+//         }
+        
+//         static bool IsPalindrome(int num)
+//         {
+//             if (num >= 0 && num <10)
+//                 return true;
+//             int numLength = GetLength(num);
+//             int[] digits = new int[numLength];
+//             for (int i = numLength - 1; i >= 0; i--) {
+//                 digits[i] = num % 10;
+//                 num /= 10;
+//             }
+//             for (int i = 0; i < numLength/2; i++) {
+//                 if (digits[i] != digits[numLength - i - 1])
+//                     return false;
+//             }
+//             return true;
+//         }
+        
+//         static int GetLength(int num)
+//         {
+//             int n = 0;
+//             while (num > 0) {
+//                 num /= 10;
+//                 n++;
+//             }
+//             return n;
+//         }
+//     }
+// }
+
+// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+// Console.Write("Введите координаты Х1: ");
+// int x1 = int.Parse(Console.ReadLine());
+// Console.Write("Введите координаты У1: ");
+// int y1 = int.Parse(Console.ReadLine());
+// Console.Write("Введите координаты z1: ");
+// int z1 = int.Parse(Console.ReadLine());
+// Console.Write("Введите координаты Х2: ");
+// int x2 = int.Parse(Console.ReadLine());
+// Console.Write("Введите координаты У2: ");
+// int y2 = int.Parse(Console.ReadLine());
+// Console.Write("Введите координаты z2: ");
+// int z2 = int.Parse(Console.ReadLine());
+// int A = (x2-x1)*(x2-x1);
+// int B = (y2-y1)*(y2-y1);
+// int C = (z2-z1)*(z2-z1);
+// double AB = Math.Sqrt(A+B+C);
+// Console.Write(AB);
+
+
+// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// Console.Write("Введите число: ");
+// int N = int.Parse(Console.ReadLine());
+// int x = 1;
+// while (x <= N)
+// {
+//     int y = x*x*x;
+//     Console.WriteLine(y);
+//     x++;
+// }
